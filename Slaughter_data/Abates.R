a44555cf3b393dcd352bb8d585053bf6f493b279
Maria Oliveira <71852526+Maria-M-Oliveira@users.noreply.github.com>
2022-11-15 14:13:51 +0000

    Add files via upload

--- Slaughter_data/Abates.R
+++ Slaughter_data/Abates.R
@@ -5,7 +5,7 @@ library(tidyverse)
 Abates <- fread("Abates.csv")
 # 3 tipos diferentes de abates: Normal, Sanitário, Emergência
 # Abates$MMA_CLA_CAR_GR corresponde a camada gordura
-# Abates$MMA_CLA_CAR_CF corresponde a classificacao da carcaça na escala EUROP
+# Abates$MMA_CLA_CAR_CF corresponde a classificacao da carcaça na escala SEUROP
 # Abates$MMA_CLA_CAR_CT corrresponde a classificação de acordo com idade e genero
 # Regulamento 1308/2013 para verificar
 # TL;DR: escalas ordinais 
@@ -16,4 +16,6 @@ Abates <- fread("Abates.csv")
 
 # Analises sugeridas:
 # Peso por raça
-# 
+# Numero abates por mes e ano + kg por mes e ano por matadouro
+# Relaçao matadouro com tipo de abate e dps estratificar mais em raça 
+# Relaçao Raça com classificação; raça com tipo de abate
